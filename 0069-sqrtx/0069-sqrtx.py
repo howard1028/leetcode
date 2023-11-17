6c5d097a46304e297c6541a71765f0fd2e088b61
howard1028 <62832922+howard1028@users.noreply.github.com>
2023-11-18 01:52:17 +0800

    Time: 41 ms (74.21%), Space: 16.1 MB (63.89%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.py
+++ 0069-sqrtx/0069-sqrtx.py
@@ -4,7 +4,7 @@ class Solution:
         lo = 1
         hi = x
         
-        while lo <= hi:
+        while lo <= hi: # 最後一個要check
             mid = (lo + hi) // 2
             power = mid * mid
             if power == x:
@@ -13,4 +13,4 @@ class Solution:
                 hi = mid - 1
             else:
                 lo = mid + 1
-        return hi
+        return hi # return ceil
