d6510e2dd445bd3126e44498657c3343f5d3dce1
howard1028 <62832922+howard1028@users.noreply.github.com>
2024-02-06 18:42:47 +0800

    Time: 79 ms (94.96%), Space: 20.2 MB (69.69%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -1,10 +1,18 @@
+from collections import defaultdict
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        
-        dict = defaultdict(list)
-        for string in strs:
-            count = [0] * 26
-            for char in string:
-                count[ord(char)-ord("a")] += 1
-            dict[tuple(count)].append(string) #list轉成tuple才能當index
-        return dict.values()
+        if len(strs) == 0:
+            return [[""]]
+        elif len(strs) == 1:
+            return [strs]
+        else:
+            res = []
+            dic = defaultdict(list)
+            for string in strs:
+                s = ''.join(sorted(string)) # 字串排序好當key
+                dic[s].append(string)
+                # print(dic)
+            dic = sorted(dic.items(), key=lambda d: d[0])
+            for key, value in dic:
+                res.append(value)
+            return res
