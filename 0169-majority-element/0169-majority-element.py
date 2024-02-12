8901fef846118ae00c84bc8b4b43ee73b83f23f5
howard1028 <62832922+howard1028@users.noreply.github.com>
2024-02-13 03:09:27 +0800

    Time: 143 ms (35.10%), Space: 17.9 MB (95.97%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -1,21 +1,10 @@
+from collections import Counter
+import operator
+
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        
-        count = {}
-        for i in nums:
-            if i in count:
-                count[i] += 1
-            else:
-                count[i] = 1
-
-        majority_time = float("-inf") # 出現次數
-        majority = 0 # 出現次數最多的數字
-
-        print(count)
-        for key , value in count.items(): # 要用.items()取出key-value鍵值對
-            print(key , value)
-            if value > majority_time:
-                majority = key
-                majority_time = value
-    
-        return majority
+        c = Counter(nums)
+        # max(c.items() , key = lambda x:x[1]) return一組items(3,2)
+        # return max(c.items() , key = lambda x:x[1])[0] 
+                
+        return max(c.items(), key=operator.itemgetter(1))[0]
