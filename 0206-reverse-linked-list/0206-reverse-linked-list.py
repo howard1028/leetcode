e4829df291ca5722761e07401159ded4bd25f1e4
howard1028 <62832922+howard1028@users.noreply.github.com>
2024-03-21 11:51:44 +0800

    Time: 37 ms (58.28%), Space: 17.7 MB (77.25%) - LeetHub

--- 0206-reverse-linked-list/0206-reverse-linked-list.py
+++ 0206-reverse-linked-list/0206-reverse-linked-list.py
@@ -7,26 +7,26 @@ class Solution:
     def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
         # # recursive 遞迴
 
-        # 結束條件
-        if not head or head.next is None:
-            return head
-        # head 跑到最後
-        new_head = self.reverseList(head.next)
-        # reverse
-        head.next.next = head
-        head.next = None
+#         # 結束條件
+#         if not head or head.next is None:
+#             return head
+#         # head 跑到最後
+#         new_head = self.reverseList(head.next)
+#         # reverse
+#         head.next.next = head
+#         head.next = None
 
-        return new_head
+#         return new_head
 
-        # # iteration 迭代
+        # iteration 迭代
 
-        # prev = None
-        # curr = head
+        prev = None
+        curr = head
 
-        # while curr is not None:
-        #     nextNode = curr.next
-        #     curr.next = prev
-        #     prev = curr
-        #     curr = nextNode 
+        while curr is not None:
+            nextNode = curr.next
+            curr.next = prev
+            prev = curr
+            curr = nextNode 
         
-        # return prev
+        return prev
