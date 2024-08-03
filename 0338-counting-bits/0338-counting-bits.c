be40491b166daa0ec237d86112713a6d19fa44f3
howard1028 <62832922+howard1028@users.noreply.github.com>
2024-08-03 23:29:55 +0800

    Time: 45 ms (18.84%), Space: 14.8 MB (23.2%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.c
+++ 0338-counting-bits/0338-counting-bits.c
@@ -2,11 +2,12 @@
  * Note: The returned array must be malloced, assume caller calls free().
  */
 int* countBits(int n, int* returnSize) {
-    int* res = (int*) malloc((n+1) * sizeof(int)); // 指標是通用型別的 void*。為了將其轉換為指向整數類型的指針，使用了類型轉換 (int*)
-    *returnSize = n + 1; // 回傳陣列的size
+    int *res = (int *) malloc(sizeof(int) * (n+1));
+    *returnSize = n + 1;
     res[0] = 0;
-    for (int i = 1; i <= n; i++){
-        res[i] = res[i >> 1] + (i & 1);
+
+    for (int i=0; i < n+1 ; i++){
+        res[i] = res[i>>1] + (i&1);
     }
     return res;
-} 
+}
