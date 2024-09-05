4014cc706fd4a676dff577c2e3daa3a5b703d7d5
howard1028 <62832922+howard1028@users.noreply.github.com>
2024-09-06 03:01:49 +0800

    Time: 3 ms (64.14%), Space: 10.5 MB (53.17%) - LeetHub

--- 0206-reverse-linked-list/0206-reverse-linked-list.c
+++ 0206-reverse-linked-list/0206-reverse-linked-list.c
@@ -6,28 +6,30 @@
  * };
  */
 struct ListNode* reverseList(struct ListNode* head) {
-    // struct ListNode *prev_node = NULL;
-    // struct ListNode *curr_node = head;
-    // struct ListNode *next_node = NULL;
 
-    // while (curr_node != NULL) {
+    // iteration
+    // struct ListNode* curr_node = head;
+    // struct ListNode* prev_node = NULL;
+    // struct ListNode* next_node = NULL;
+
+    // while (curr_node != NULL){
     //     next_node = curr_node -> next;
     //     curr_node -> next = prev_node;
+
     //     prev_node = curr_node;
     //     curr_node = next_node;
     // }
     // return prev_node;
 
-    struct ListNode* curr_node = head;
-    struct ListNode* prev_node = NULL;
-    struct ListNode* next_node = NULL;
-
-    while (curr_node != NULL){
-        next_node = curr_node -> next;
-        curr_node -> next = prev_node;
-
-        prev_node = curr_node;
-        curr_node = next_node;
+    // recursion
+    // 終止條件：只有一個node
+    if (head == NULL || head->next == NULL){
+        return head;
     }
-    return prev_node;
+    // 後面進入遞迴
+    struct ListNode* newHead = reverseList(head->next);
+    
+    head->next->next = head;
+    head->next = NULL;
+    return newHead;
 }
