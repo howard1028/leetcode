514a07dcf7a4f86f2ec5956946c83b1e121185ec
howard1028 <62832922+howard1028@users.noreply.github.com>
2024-09-27 18:09:33 +0800

    Time: 13 ms (52%), Space: 11.4 MB (58.37%) - LeetHub

--- 1580-shuffle-the-array/1580-shuffle-the-array.c
+++ 1580-shuffle-the-array/1580-shuffle-the-array.c
@@ -2,11 +2,11 @@
  * Note: The returned array must be malloced, assume caller calls free().
  */
 int* shuffle(int* nums, int numsSize, int n, int* returnSize){
-    int* result = (int*) malloc(sizeof(int) * numsSize);
+    int* res = (int*)malloc(sizeof(int) * numsSize);
     *returnSize = numsSize;
-    for(int i=0, j=n, k=0; i<n; i++,j++){
-        result[k++] = nums[i];
-        result[k++] = nums[j];
+    for (int i=0 , j=n , k=0; i<n; i++ , j++){
+        res[k++] = nums[i];
+        res[k++] = nums[j]; 
     }
-    return result;
+    return res;
 }
