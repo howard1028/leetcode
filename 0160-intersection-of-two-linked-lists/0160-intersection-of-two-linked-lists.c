804bf84e3270287740b96bc01212080ee6ec0c19
howard1028 <62832922+howard1028@users.noreply.github.com>
2024-10-11 02:06:41 +0800

    Time: 42 ms (29.15%), Space: 17.4 MB (6.78%) - LeetHub

---
+++ 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.c
@@ -0,0 +1,24 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+struct ListNode *getIntersectionNode(struct ListNode *headA, struct ListNode *headB) {
+    if (headA == NULL || headB == NULL){
+        return NULL;
+    }
+    struct ListNode* pA = headA;
+    struct ListNode* pB = headB;
+    // 有交叉：同時指到某個node
+    // 沒交叉：同時指到NULL
+    while (pA != pB){
+        if (pA == NULL && pB == NULL){
+            break;
+        }
+        pA = (pA == NULL) ? headB : pA->next;
+        pB = (pB == NULL) ? headA : pB->next;
+    }
+    return pA;
+}
